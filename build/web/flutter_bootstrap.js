6bf8da2906030404c53895e594c47920ca84888c
brian <brianburgers47@gmail.com>
2026-04-16 23:08:32 +0200

    Update paywall and checkout UI

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "860959892" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2084335775" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
